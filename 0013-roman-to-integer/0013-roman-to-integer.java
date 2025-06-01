bfa044f52f471834ad9b08be9611aa3f0980d585
V.BalaAdithya <vacchaadithya@gmail.com>
2025-06-01 16:59:07 +0530

    Time: 3 ms (79.97%), Space: 44.9 MB (36.52%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,5 +1,59 @@
 class Solution {
     public int romanToInt(String s) {
-        
+        int ans=0;
+    for(int i=0;i<s.length();i++){
+        if(s.charAt(i)=='I'){
+            if(i!=s.length()-1 && s.charAt(i+1)=='V'){
+                ans+=4;
+                i+=1;
+            }
+            else if(i!=s.length()-1 && s.charAt(i+1)=='X'){
+                ans+=9;
+                i+=1;
+            }
+            else{
+            ans+=1;
+            }
+        }
+        else if(s.charAt(i)=='V'){
+            ans+=5;
+        }
+        else if(s.charAt(i)=='X'){
+            if(i!=s.length()-1 && s.charAt(i+1)=='L'){
+                ans+=40;
+                i+=1;
+            }
+            else if(i!=s.length()-1 && s.charAt(i+1)=='C'){
+                ans+=90;
+                i+=1;
+            }
+            else{
+            ans+=10;
+            }
+        }
+        else if(s.charAt(i)=='L'){
+            ans+=50;
+        }
+        else if(s.charAt(i)=='C'){
+            if(i!=s.length()-1 && s.charAt(i+1)=='D'){
+                ans+=400;
+                i+=1;
+            }
+            else if(i!=s.length()-1 && s.charAt(i+1)=='M'){
+                ans+=900;
+                i+=1;
+            }
+            else{
+            ans+=100;
+            }
+        }
+        else if(s.charAt(i)=='D'){
+            ans+=500;
+        }
+        else{
+            ans+=1000;
+        }
+    }
+    return ans;
     }
 }
