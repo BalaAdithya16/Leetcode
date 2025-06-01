b7954d10e8e0da24c70b153b41b1d9e77819aef8
V.BalaAdithya <vacchaadithya@gmail.com>
2025-06-01 17:04:14 +0530

    Time: 9 ms (6.44%), Space: 42 MB (10.76%) - LeetHub

---
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        HashMap<Character,Integer> hm=new HashMap<>();
+        for(int i=97;i<=122;i++){
+            char c=(char)i;
+            hm.put(c,hm.getOrDefault(c,0)+1);
+        }
+        for(int i=0;i<sentence.length();i++){
+            char c=sentence.charAt(i);
+            if(hm.containsKey(c)){
+                hm.put(c,hm.get(c)+1);
+            }
+        }
+        for(char i:hm.keySet()){
+            if(hm.get(i)==1){
+                return false;
+            }
+        }
+        return true;
+    }
+}
