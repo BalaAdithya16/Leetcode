d19e209dfdcca8cde34082338d23d3d9c407626d
V.BalaAdithya <vacchaadithya@gmail.com>
2025-06-19 10:35:36 +0530

    Time: 16 ms (93.52%), Space: 58.3 MB (61.77%) - LeetHub

--- 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.java
@@ -1,38 +1,39 @@
 class Solution {
-    public static boolean posi(int[] bd,int m,int k,int mid){
-       int count=0;
-       for(int i=0;i<bd.length;i++){
-        if(bd[i]<=mid){
-            count++;
+    public boolean isposi(int[] bloo,int m,int k,int n){
+        int temp=k;
+        for(int r=0;r<bloo.length;r++){
+            if(bloo[r]<=n){
+                k-=1;
+                if(k==0){
+                    m-=1;
+                    k=temp;
+                    if(m==0){
+                        return true;
+                    }
+                } 
+            }
+            else{
+                k=temp;
+            }
         }
-        else{
-            count=0;
-        }
-        if(count==k){
-            count=0;
-            m-=1;
-        }
-       if(m==0){
-        return true;
-       }
-       }
-       return false;
+        return false;
     }
     public int minDays(int[] bloomDay, int m, int k) {
-        int l=1;
+        int l=0;
         int r=(int)Math.pow(10,9);
         while(l<=r){
             int mid=l+(r-l)/2;
-            if(posi(bloomDay,m,k,mid)){
+            if(isposi(bloomDay,m,k,mid)){
                 r=mid-1;
             }
             else{
                 l=mid+1;
             }
         }
-        if(l>Math.pow(10,9)){
+        if(l>(int)Math.pow(10,9)){
             return -1;
         }
         return l;
+
     }
 }
